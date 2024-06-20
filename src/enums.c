dd39ed0e429b2d6aa45770b572fb1543bfe4bc8e
sranucci <sranucci@itba.edu.ar>
2024-06-20 17:32:05 -0300

    Minor Formating

--- src/enums.c
+++ src/enums.c
@@ -3,28 +3,30 @@
 
 #include "include/enums.h"
 
-program_mode get_program_mode(char * mode_str){
+program_mode get_program_mode(char *mode_str)
+{
     if (!strcmp(mode_str, "-embed"))
         return EMBED_MODE;
     else if (!strcmp(mode_str, "-extract"))
         return EXTRACT_MODE;
-    
+
     return UNSPECIFIED_MODE;
 }
 
-
-steg_alg get_steg_alg(char * steg_str){
+steg_alg get_steg_alg(char *steg_str)
+{
     if (!strcmp(steg_str, "LSB1"))
         return LSB1;
     else if (!strcmp(steg_str, "LSB4"))
         return LSB4;
     else if (!strcmp(steg_str, "LSBI"))
         return LSBI;
-    
+
     return UNSPECIFIED_STEG;
 }
 
-encryption_alg get_encryption_alg(char * encryption_str){
+encryption_alg get_encryption_alg(char *encryption_str)
+{
     if (!strcmp(encryption_str, "AES128"))
         return AES128;
     else if (!strcmp(encryption_str, "AES192"))
@@ -33,11 +35,11 @@ encryption_alg get_encryption_alg(char * encryption_str){
         return AES256;
     else if (!strcmp(encryption_str, "DES"))
         return DES;
-    
     return UNSPECIFIED_ENC;
 }
 
-block_chaining_mode get_chaining_mode(char * chaining_str){
+block_chaining_mode get_chaining_mode(char *chaining_str)
+{
     if (!strcmp(chaining_str, "CBC"))
         return CBC;
     else if (!strcmp(chaining_str, "ECB"))
@@ -46,6 +48,6 @@ block_chaining_mode get_chaining_mode(char * chaining_str){
         return CFB;
     else if (!strcmp(chaining_str, "OFB"))
         return OFB;
-    
+
     return UNSPECIFIED_CHAIN;
 }
--- src/extract/extract.c
+++ src/extract/extract.c
@@ -12,32 +12,36 @@
 #define REQUIRED_PARAMS_NO 3
 #define REQUIRED_ARGS_NO REQUIRED_PARAMS_NO * 2
 
-struct params {
+struct params
+{
     // Required
-    char * p_bitmap_file;
-    char * out_bitmap_file;
+    char *p_bitmap_file;
+    char *out_bitmap_file;
     steg_alg steg;
     // Optional
-    encryption_alg encryption; // default = AES128
+    encryption_alg encryption;    // default = AES128
     block_chaining_mode chaining; // default = CBC
-    char * password;
+    char *password;
 };
 
-status_code set_params_extract(struct params * params, int argc, char * argv[]);
-status_code set_param_extract(struct params * params, char * arg_name, char * arg_value);
+status_code set_params_extract(struct params *params, int argc, char *argv[]);
+status_code set_param_extract(struct params *params, char *arg_name, char *arg_value);
 
-status_code extract(int argc, char * argv[]){
+status_code extract(int argc, char *argv[])
+{
     // Initialize params
     struct params params = {0};
 
     // Set params
     status_code status = set_params_extract(&params, argc, argv);
-    if (status != SUCCESS){
+    if (status != SUCCESS)
+    {
         print_error("Error in -extract params\n");
         return status;
     }
 
-    switch (params.steg){
+    switch (params.steg)
+    {
     case LSB1:
         extract_lsb1(params.p_bitmap_file, params.out_bitmap_file);
         break;
@@ -50,54 +54,63 @@ status_code extract(int argc, char * argv[]){
         return INVALID_STEG_ALG;
     }
 
-
     return SUCCESS;
 }
 
-status_code set_params_extract(struct params * params, int argc, char * argv[]){
+status_code set_params_extract(struct params *params, int argc, char *argv[])
+{
     // Check correct number
     if (argc < REQUIRED_ARGS_NO || argc % 2 == 1)
         return ILLEGAL_ARGUMENTS;
 
     // Set params struct
-    for (int i = 0; i < argc; i+=2){
-        int status = set_param_extract(params, argv[i], argv[i+1]);
+    for (int i = 0; i < argc; i += 2)
+    {
+        int status = set_param_extract(params, argv[i], argv[i + 1]);
         if (status != SUCCESS)
             return status;
     }
 
     // Check that the struct is valid
-    if ( params->p_bitmap_file == NULL || params->out_bitmap_file == NULL || params->steg == UNSPECIFIED_STEG)
+    if (params->p_bitmap_file == NULL || params->out_bitmap_file == NULL || params->steg == UNSPECIFIED_STEG)
         return ILLEGAL_ARGUMENTS;
 
     return SUCCESS;
 }
 
-status_code set_param_extract(struct params * params, char * arg_name, char * arg_value){
+status_code set_param_extract(struct params *params, char *arg_name, char *arg_value)
+{
     bool error_ocurred = false;
-    
-    if (!strcmp(arg_name, "-p")){
+
+    if (!strcmp(arg_name, "-p"))
+    {
         params->p_bitmap_file = arg_value;
     }
-    else if (!strcmp(arg_name, "-out")){
+    else if (!strcmp(arg_name, "-out"))
+    {
         params->out_bitmap_file = arg_value;
     }
-    else if (!strcmp(arg_name, "-steg")){
+    else if (!strcmp(arg_name, "-steg"))
+    {
         params->steg = get_steg_alg(arg_value);
         error_ocurred = (params->steg == UNSPECIFIED_STEG);
     }
-    else if (!strcmp(arg_name, "-a")){
+    else if (!strcmp(arg_name, "-a"))
+    {
         params->encryption = get_encryption_alg(arg_value);
         error_ocurred = (params->encryption == UNSPECIFIED_ENC);
     }
-    else if (!strcmp(arg_name, "-m")){
+    else if (!strcmp(arg_name, "-m"))
+    {
         params->chaining = get_chaining_mode(arg_value);
         error_ocurred = (params->chaining == UNSPECIFIED_CHAIN);
     }
-    else if (!strcmp(arg_name, "-pass")){
+    else if (!strcmp(arg_name, "-pass"))
+    {
         params->password = arg_value;
     }
-    else {
+    else
+    {
         error_ocurred = true;
     }
 
