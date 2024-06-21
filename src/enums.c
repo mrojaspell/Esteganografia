d55e1f3f50c832e21afaa0f22cf9ff33d9e5d34d
Juan Ramiro Castro <juancastro@itba.edu.ar>
2024-06-21 20:21:02 -0300

    FIX: Make args ignore case in enums

--- src/enums.c
+++ src/enums.c
@@ -5,9 +5,9 @@
 
 program_mode get_program_mode(char *mode_str)
 {
-    if (!strcmp(mode_str, "-embed"))
+    if (!strcasecmp(mode_str, "-embed"))
         return EMBED_MODE;
-    else if (!strcmp(mode_str, "-extract"))
+    if (!strcasecmp(mode_str, "-extract"))
         return EXTRACT_MODE;
 
     return UNSPECIFIED_MODE;
@@ -15,11 +15,11 @@ program_mode get_program_mode(char *mode_str)
 
 steg_alg get_steg_alg(char *steg_str)
 {
-    if (!strcmp(steg_str, "LSB1"))
+    if (!strcasecmp(steg_str, "LSB1"))
         return LSB1;
-    else if (!strcmp(steg_str, "LSB4"))
+    if (!strcasecmp(steg_str, "LSB4"))
         return LSB4;
-    else if (!strcmp(steg_str, "LSBI"))
+    if (!strcasecmp(steg_str, "LSBI"))
         return LSBI;
 
     return UNSPECIFIED_STEG;
@@ -27,26 +27,26 @@ steg_alg get_steg_alg(char *steg_str)
 
 encryption_alg get_encryption_alg(char *encryption_str)
 {
-    if (!strcmp(encryption_str, "AES128"))
+    if (!strcasecmp(encryption_str, "AES128"))
         return AES128;
-    else if (!strcmp(encryption_str, "AES192"))
+    if (!strcasecmp(encryption_str, "AES192"))
         return AES192;
-    else if (!strcmp(encryption_str, "AES256"))
+    if (!strcasecmp(encryption_str, "AES256"))
         return AES256;
-    else if (!strcmp(encryption_str, "DES"))
+    if (!strcasecmp(encryption_str, "DES"))
         return DES;
     return AES128;
 }
 
 block_chaining_mode get_chaining_mode(char *chaining_str)
 {
-    if (!strcmp(chaining_str, "CBC"))
+    if (!strcasecmp(chaining_str, "CBC"))
         return CBC;
-    else if (!strcmp(chaining_str, "ECB"))
+    if (!strcasecmp(chaining_str, "ECB"))
         return ECB;
-    else if (!strcmp(chaining_str, "CFB"))
+    if (!strcasecmp(chaining_str, "CFB"))
         return CFB;
-    else if (!strcmp(chaining_str, "OFB"))
+    if (!strcasecmp(chaining_str, "OFB"))
         return OFB;
     return CBC;
 }
